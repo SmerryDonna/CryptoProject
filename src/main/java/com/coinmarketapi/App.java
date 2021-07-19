ae5ebb700f2cddc224ae6237cd0639940648b700
Salty Spaghetti <matteoterzuolo@gmail.com>
2021-07-19 15:42:03 +0200

    Aggiunte classi per richiamare le API di Coin Market Cap

---
+++ src/main/java/com/coinmarketapi/App.java
@@ -0,0 +1,13 @@
+import models.ApiResponse;
+
+public class App {
+    public static void main(String[] args) {
+        ApiResponse<Double> response = ApiManager.getBitcoinPrice();
+
+        if (response.getSuccess()) {
+            System.out.println("Il prezzo di Bitcoin e' di: " + response.getData() + " euro");
+        } else {
+            System.out.println("E' andato tutto in vacca");
+        }
+    }
+}
