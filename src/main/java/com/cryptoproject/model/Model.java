211628450b990af8fe0a2cabddcd2aedc794f4aa
StefanoAlcamo <stefanoacamo1@gmail.com>
2021-08-24 15:31:32 +0200

    modello aggiornato

--- src/main/java/com/cryptoproject/model/Model.java
+++ src/main/java/com/cryptoproject/model/Model.java
@@ -1,9 +1,60 @@
 package com.cryptoproject.model;
 
+import java.awt.event.ActionListener;
+
 public class Model {
+	
+
+    private String indirizzoProtafoglio;
+	private String previsione;
+	private String valuta;
+	private String scommessa;
+	
+	private String username;
+	private String psw;
+
 
 	public Model() {
 		
+		this.indirizzoProtafoglio = null;
+		this.previsione = null;
+		this.valuta = null;
+		this.scommessa = null;
 		
+		this.username = null;
+		this.psw = null;
 	}
+	
+	public void setIndirizzo(String indirizzoPortafoglio)
+	{
+		this.indirizzoProtafoglio = indirizzoPortafoglio;
+	}
+	
+	public void setPrevisione(String previsione)
+	{
+		this.previsione = previsione;
+	}
+	
+	public void setValuta(String valuta)
+	{
+		this.valuta = valuta;
+	}
+	
+	public void setScommessa(String scommessa)
+	{
+		this.scommessa = scommessa;
+	}
+	
+	
+	
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	
+	public void setPsw(String psw)
+	{
+		this.psw = psw;
+	}
+	
 }
