4f55a41f502a5e774e916d7da30031a960eda2f4
SmerryDonna <smerry1998@gmail.com>
2021-08-26 16:38:43 +0200

    Tolto cose che non servivano, sistemate dependencies

--- src/main/java/com/coinmarketapi/ApiManager.java
+++ src/main/java/com/coinmarketapi/ApiManager.java
@@ -1,11 +1,10 @@
 package com.coinmarketapi;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -15,70 +14,72 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
+import org.springframework.http.HttpHeaders;
 
 import com.coinmarketapi.helpers.Helper;
 import com.coinmarketapi.models.ApiResponse;
 
 public class ApiManager {
-    private static String apiKey = "839fd452-f13c-46ac-80b2-0ee569437ec9";
+	private static String apiKey = "839fd452-f13c-46ac-80b2-0ee569437ec9";
 
-    public static ApiResponse<Double> getBitcoinPrice() {
-        String uri = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
-        List<NameValuePair> parameters = new ArrayList<NameValuePair>();
-        parameters.add(new BasicNameValuePair("id", "1"));
-        parameters.add(new BasicNameValuePair("convert", "EUR"));
+	public static ApiResponse<Double> getBitcoinPrice() {
+		String uri = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
+		List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+		parameters.add(new BasicNameValuePair("id", "1"));
+		parameters.add(new BasicNameValuePair("convert", "EUR"));
 
-        ApiResponse<Double> result = new ApiResponse<Double>(false, null);
+		ApiResponse<Double> result = new ApiResponse<Double>(false, null);
 
-        try {
-            ApiResponse<String> response = makeAPICall(uri, parameters);
-            boolean success = (boolean) response.getSuccess();
+		try {
+			ApiResponse<String> response = makeAPICall(uri, parameters);
+			boolean success = response.getSuccess();
 
-            if (!success) return result;
+			if (!success)
+				return result;
 
-            JSONObject responseContent = new JSONObject(response.getData());
-            JSONObject bitcoinObject = responseContent.getJSONObject("data").getJSONObject("1");
+			JSONObject responseContent = new JSONObject(response.getData());
+			JSONObject bitcoinObject = responseContent.getJSONObject("data").getJSONObject("1");
 
-            double price = bitcoinObject.getJSONObject("quote").getJSONObject("EUR").getDouble("price");
-            
-            result.setSuccess(success);
-            result.setData(price);
-        } catch (IOException | URISyntaxException e) {
-            return result;
-        }
+			double price = bitcoinObject.getJSONObject("quote").getJSONObject("EUR").getDouble("price");
 
-        return result;
-    }
+			result.setSuccess(success);
+			result.setData(price);
+		} catch (IOException | URISyntaxException e) {
+			return result;
+		}
 
-    private static ApiResponse<String> makeAPICall(String uri, List<NameValuePair> parameters)
-            throws URISyntaxException, IOException {
+		return result;
+	}
 
-        String response_content = "";
+	private static ApiResponse<String> makeAPICall(String uri, List<NameValuePair> parameters)
+			throws URISyntaxException, IOException {
 
-        URIBuilder query = new URIBuilder(uri);
-        query.addParameters(parameters);
+		String response_content = "";
 
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpGet request = new HttpGet(query.build());
+		URIBuilder query = new URIBuilder(uri);
+		query.addParameters(parameters);
 
-        request.setHeader(HttpHeaders.ACCEPT, "application/json");
-        request.addHeader("X-CMC_PRO_API_KEY", apiKey);
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpGet request = new HttpGet(query.build());
 
-        CloseableHttpResponse response = client.execute(request);
+		request.setHeader(HttpHeaders.ACCEPT, "application/json");
+		request.addHeader("X-CMC_PRO_API_KEY", apiKey);
 
-        //Check if HTTP call worked
-        int statusCode = response.getStatusLine().getStatusCode();
-        boolean goodResponse = Helper.checkHTTPStatus(statusCode);
+		CloseableHttpResponse response = client.execute(request);
 
-        try {
-            HttpEntity entity = response.getEntity();
-            response_content = EntityUtils.toString(entity);
-            EntityUtils.consume(entity);
-        } finally {
-            response.close();
-        }
+		// Check if HTTP call worked
+		int statusCode = response.getStatusLine().getStatusCode();
+		boolean goodResponse = Helper.checkHTTPStatus(statusCode);
 
-        return new ApiResponse<String>(goodResponse, response_content);
-    }
+		try {
+			org.apache.http.HttpEntity entity = response.getEntity();
+			response_content = EntityUtils.toString(entity);
+			EntityUtils.consume(entity);
+		} finally {
+			response.close();
+		}
+
+		return new ApiResponse<String>(goodResponse, response_content);
+	}
 
 }
